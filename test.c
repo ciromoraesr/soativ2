b42ab9bffd0c2a0a7f3509bd12d36b16cb9ceed1
apodreciro <ciromoraes.r@gmail.com>
2024-12-03 17:47:30 -0300

    a

--- test.c
+++ test.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <limits.h> 
+#include <pthread.h>
 
 int count_files(const char *path) {
     int count = 0;
@@ -23,9 +24,29 @@ int count_files(const char *path) {
     return count;
 }
 
+void count_ocur(const char *path, const char *text){
+    FILE *p = fopen(path, "r");
+    char str[1000];
+    char *pos;
+    int i, count = 0;
+    
+    while((fgets(str, 1000, p)) != NULL){
+        i = 0;
+        while ((pos = strstr(str+i, text)) != NULL){
+            i = (pos - str) + 1;
+            count++;
+        
+        }
+    }
+    printf("%d ocurrences found in %s", count, path);
+
+
+}
 void monitor_directory(const char *path, const char *text) {
     int size = count_files(path);
-    
+    pthread_t t0, t1, t2, t3, t4, t5, t6, t7, t8, t9;
+    pthread_t fila[10];
+
     if (size < 0) {
         return; 
     }
@@ -35,7 +56,7 @@ void monitor_directory(const char *path, const char *text) {
         return;
     }
     memset(vat, 0, size * sizeof(time_t)); 
-
+       
     while (1) {
         int size1 = count_files(path);
         if (size1 < 0) {
@@ -85,8 +106,10 @@ void monitor_directory(const char *path, const char *text) {
                 if (vat[count] != path_stat.st_mtime) {
                     printf("Arquivo '%s' modificado.\n", entry->d_name);
                     //aqui entraria uma thread operaria eu imagino
+                    
                     vat[count] = path_stat.st_mtime;
                 } else {
+                    //count_ocur(full_path, text);
                     printf("'%s' Dboa .\n", entry->d_name);
                 }
                 count++;
